098b8d7fd15f693ebdd578e4807b5fbe39cc54ec
Jiri Stransky <jistr@redhat.com>
2014-07-30 17:27:04 +0200

    BZ#1122701 - Incoming ports are wide open
    https://bugzilla.redhat.com/show_bug.cgi?id=1122701
    
    After disabling firewalld, the system has no firewall rules. We need
    to recreate the basic firewall rules:
    
    * allow ICMP,
    
    * allow loopback,
    
    * allow packets with related/established connection state,
    
    * allow ssh,
    
    * reject at the end of input/forward chains.

--- puppet/modules/quickstack/manifests/openstack_common.pp
+++ puppet/modules/quickstack/manifests/openstack_common.pp
@@ -7,11 +7,46 @@ class quickstack::openstack_common {
           ensure => present, }
   }
 
-  # Stop firewalld since everything uses iptables
-  # for now (same as packstack did)
-  service { "firewalld":
-    ensure => "stopped",
-    enable => false,
+  # for any Fedora, and for Enterprise Linux > 6
+  if ($::operatingsystem == 'Fedora' or
+      ($::operatingsystem != 'Fedora' and $::operatingsystemmajrelease > 6)) {
+    # Stop firewalld since everything uses iptables for now (same as
+    # packstack did), and recreate the basic rules
+    service { "firewalld":
+      ensure => "stopped",
+      enable => false,
+    } ->
+    firewall { '000 icmp':
+      proto  => 'icmp',
+      action => 'accept',
+    } ->
+    firewall { '000 related and established':
+      proto   => 'all',
+      ctstate => ['RELATED', 'ESTABLISHED'],
+      action  => 'accept',
+    } ->
+    firewall { '000 loopback':
+      proto   => 'all',
+      iniface => 'lo',
+      action  => 'accept',
+    } ->
+    firewall { '010 ssh':
+      proto  => 'tcp',
+      ctstate => ['NEW'],
+      dport  => 22,
+      action => 'accept',
+    } ->
+    firewall { '999 reject all':
+      proto  => 'all',
+      action => 'reject',
+      reject => 'icmp-host-prohibited',
+    } ->
+    firewall { '999 fwd reject all':
+      chain  => 'FORWARD',
+      proto  => 'all',
+      action => 'reject',
+      reject => 'icmp-host-prohibited',
+    }
   }
 
   service { "auditd":
